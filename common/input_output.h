0941834fb8c2cd8dc0e3d74df3bb0966c49ccf28
Arman Pazouki <pazouki@wisc.edu>
2018-02-16 13:46:14 -0800

    modify the code to work with the latest chrono. remove all the zlib stuff.

--- common/input_output.h
+++ common/input_output.h
@@ -8,8 +8,8 @@
 #include <sstream>
 #include <cstring>
 #include <thread>
-#include <zlib.h>
-#include "chrono_parallel/lcp/ChLcpSystemDescriptorParallel.h"
+//#include <zlib.h>
+//#include "chrono_parallel/lcp/ChLcpSystemDescriptorParallel.h"
 #include "chrono_parallel/collision/ChNarrowphaseRUtils.h"
 #include <chrono_parallel/physics/ChSystemParallel.h>
 #include <chrono_parallel/physics/Ch3DOFContainer.h>
@@ -19,231 +19,231 @@
 #include "chrono/utils/ChUtilsGenerators.h"
 
 #include "chrono_opengl/ChOpenGLWindow.h"
+//
+//using namespace chrono;
+//using namespace chrono::collision;
+//using namespace chrono::geometry;
+//using namespace chrono::utils;
+//
+//using std::cout;
+//using std::endl;
+////
+//class BinaryGen {
+//  public:
+//    BinaryGen() {}
+//    void OpenFile(std::string filename) { bin_file.open(filename.c_str(), std::ios::out | std::ofstream::binary); }
+//    void CloseFile() { bin_file.close(); }
+//    template <class T>
+//    void operator<<(const std::vector<T>& data) {
+//        size_t size = data.size();
+//        // write size of data type and then the data;
+//        bin_file.write(reinterpret_cast<const char*>(&size), sizeof(size));
+//        bin_file.write(reinterpret_cast<const char*>(&data[0]), size * sizeof(T));
+//    }
+//    template <typename T>
+//    void Write(const std::vector<T>& data) {
+//        size_t size = data.size();
+//        bin_file.write(reinterpret_cast<const char*>(&size), sizeof(size));
+//        bin_file.write(reinterpret_cast<const char*>(&data[0]), size * sizeof(T));
+//    }
+//    std::ofstream bin_file;
+//};
 
-using namespace chrono;
-using namespace chrono::collision;
-using namespace chrono::geometry;
-using namespace chrono::utils;
+//std::vector<std::thread> writethreads;
+//
+//void WriteLocalData(const std::string&& filename,
+//                    const std::vector<real3>&& pos_particle,
+//                    const std::vector<real3>&& vel_particle,
+//                    const int&& num_particles) {
+//    BinaryGen bin_output;
+//    bin_output.OpenFile(filename.c_str());
+//    bin_output.Write(pos_particle);
+//    bin_output.Write(vel_particle);
+//    bin_output.CloseFile();
+//}
+//
+//void DumpFluidData(chrono::ChSystemParallelNSC* system, std::string filename, bool binary = true) {
+//    std::vector<real3> pos_particle;
+//    std::vector<real3> vel_particle;
+//    int num_particles = system->data_manager->num_fluid_bodies;
+//    if (num_particles <= 0) {
+//        std::cout << "No fluid to write!!\n";
+//        return;
+//    }
+//
+//    pos_particle = system->data_manager->host_data.pos_3dof;
+//    vel_particle = system->data_manager->host_data.vel_3dof;
+//
+//    writethreads.push_back(std::thread(WriteLocalData, std::move(filename), std::move(pos_particle),
+//                                       std::move(vel_particle), std::move(num_particles)));
+//    if (writethreads.size() > 8) {
+//        for (std::thread& t : writethreads) {
+//            t.join();
+//        }
+//        writethreads.clear();
+//    }
+//}
+//void WriteStringData(const std::string&& filename, const std::string&& ss) {
+//    gzFile gz_file = gzopen(filename.c_str(), "wb");
+//    unsigned long int file_size = sizeof(char) * ss.size();
+//    // writing size of file
+//    gzwrite(gz_file, (void*)&file_size, sizeof(file_size));
+//    gzwrite(gz_file, (void*)(ss.data()), file_size);
+//    gzclose(gz_file);
+//}
 
-using std::cout;
-using std::endl;
+//void DumpStringData(std::string filename, std::string ss) {
+//    writethreads.push_back(std::thread(WriteStringData, std::move(filename), std::move(ss)));
+//    if (writethreads.size() > 8) {
+//        for (std::thread& t : writethreads) {
+//            t.join();
+//        }
+//        writethreads.clear();
+//    }
+//}
 
-class BinaryGen {
-  public:
-    BinaryGen() {}
-    void OpenFile(std::string filename) { bin_file.open(filename.c_str(), std::ios::out | std::ofstream::binary); }
-    void CloseFile() { bin_file.close(); }
-    template <class T>
-    void operator<<(const std::vector<T>& data) {
-        size_t size = data.size();
-        // write size of data type and then the data;
-        bin_file.write(reinterpret_cast<const char*>(&size), sizeof(size));
-        bin_file.write(reinterpret_cast<const char*>(&data[0]), size * sizeof(T));
-    }
-    template <typename T>
-    void Write(const std::vector<T>& data) {
-        size_t size = data.size();
-        bin_file.write(reinterpret_cast<const char*>(&size), sizeof(size));
-        bin_file.write(reinterpret_cast<const char*>(&data[0]), size * sizeof(T));
-    }
-    std::ofstream bin_file;
-};
-
-std::vector<std::thread> writethreads;
-
-void WriteLocalData(const std::string&& filename,
-                    const std::vector<real3>&& pos_particle,
-                    const std::vector<real3>&& vel_particle,
-                    const int&& num_particles) {
-    BinaryGen bin_output;
-    bin_output.OpenFile(filename.c_str());
-    bin_output.Write(pos_particle);
-    bin_output.Write(vel_particle);
-    bin_output.CloseFile();
-}
-
-void DumpFluidData(chrono::ChSystemParallelDVI* system, std::string filename, bool binary = true) {
-    std::vector<real3> pos_particle;
-    std::vector<real3> vel_particle;
-    int num_particles = system->data_manager->num_fluid_bodies;
-    if (num_particles <= 0) {
-        std::cout << "No fluid to write!!\n";
-        return;
-    }
-
-    pos_particle = system->data_manager->host_data.pos_3dof;
-    vel_particle = system->data_manager->host_data.vel_3dof;
-
-    writethreads.push_back(std::thread(WriteLocalData, std::move(filename), std::move(pos_particle),
-                                       std::move(vel_particle), std::move(num_particles)));
-    if (writethreads.size() > 8) {
-        for (std::thread& t : writethreads) {
-            t.join();
-        }
-        writethreads.clear();
-    }
-}
-void WriteStringData(const std::string&& filename, const std::string&& ss) {
-    gzFile gz_file = gzopen(filename.c_str(), "wb");
-    unsigned long int file_size = sizeof(char) * ss.size();
-    // writing size of file
-    gzwrite(gz_file, (void*)&file_size, sizeof(file_size));
-    gzwrite(gz_file, (void*)(ss.data()), file_size);
-    gzclose(gz_file);
-}
-
-void DumpStringData(std::string filename, std::string ss) {
-    writethreads.push_back(std::thread(WriteStringData, std::move(filename), std::move(ss)));
-    if (writethreads.size() > 8) {
-        for (std::thread& t : writethreads) {
-            t.join();
-        }
-        writethreads.clear();
-    }
-}
-
-class CSVGen {
-  public:
-    CSVGen() {
-        delim = ",";
-        binary = false;
-        gz_file = 0;
-    }
-    ~CSVGen() {}
-
-    void OpenFile(std::string filename, bool bin = false) {
-        if (bin) {
-            binary = true;
-            gz_file = gzopen(filename.c_str(), "wb");
-        } else {
-            ofile.open(filename.c_str(), std::ios::out);
-        }
-    }
-
-    void CloseFile() {
-        if (binary) {
-            unsigned long int file_size = sizeof(char) * ss.str().size();
-
-            gzwrite(gz_file, (void*)&file_size, sizeof(file_size));  // writing size of file
-            gzwrite(gz_file, (void*)(ss.str().data()), file_size);
-            gzclose(gz_file);
-        } else {
-            ofile << ss.str();
-            ofile.close();
-        }
-    }
-    template <class T>
-    void operator<<(const T& token) {
-        WriteToken(token);
-    }
-    void WriteToken(real token) { ss << token << delim; }
-    void WriteToken(real2 token) { ss << token.x << delim << token.y << delim; }
-    void WriteToken(real3 token) { ss << token.x << delim << token.y << delim << token.z << delim; }
-    void WriteToken(real4 token) { ss << token.x << delim << token.y << delim << token.z << delim << token.w << delim; }
-    void WriteToken(quaternion token) {
-        ss << token.w << delim << token.x << delim << token.y << delim << token.z << delim;
-    }
-    void WriteToken(ChVector<> token) { ss << token.x << delim << token.y << delim << token.z << delim; }
-    void WriteToken(ChQuaternion<> token) {
-        ss << token.e0 << delim << token.e1 << delim << token.e2 << delim << token.e3 << delim;
-    }
-    void WriteToken(std::string token) { ss << token << delim; }
-    void endline() { ss << std::endl; }
-
-    std::string delim;
-    std::ofstream ofile;
-    std::stringstream ss;
-    gzFile gz_file;
-    bool binary;
-};
-
-void DumpAllObjectsWithGeometryPovray(ChSystem* mSys, std::string filename, bool binary = false) {
-    CSVGen csv_output;
-
-    for (int i = 0; i < mSys->Get_bodylist()->size(); i++) {  //
-        auto abody = mSys->Get_bodylist()->at(i);             // Get body
-        const Vector pos = abody->GetFrame_REF_to_abs().GetPos();
-        const Vector vel = abody->GetPos_dt();  // Get velocity
-        Quaternion rot = abody->GetFrame_REF_to_abs().GetRot();
-        Vector pos_final, rad_final;                 //
-        ShapeType type = chrono::collision::SPHERE;  //
-        // Get each asset
-        for (int j = 0; j < abody->GetAssets().size(); j++) {
-            auto asset = abody->GetAssets().at(j);
-            if (!std::dynamic_pointer_cast<ChVisualization>(asset)) {
-                continue;
-            }
-            ChVisualization* visual_asset = ((ChVisualization*)(asset.get()));
-            Vector center = visual_asset->Pos;
-            center = rot.Rotate(center);
-            pos_final = pos + center;
-            Quaternion lrot = visual_asset->Rot.Get_A_quaternion();
-            lrot = rot * lrot;
-            lrot.Normalize();
-            if (std::dynamic_pointer_cast<ChSphereShape>(asset)) {
-                ChSphereShape* sphere_shape = ((ChSphereShape*)(asset.get()));
-                real radius = sphere_shape->GetSphereGeometry().rad;
-                rad_final.x = radius;
-                rad_final.y = radius;
-                rad_final.z = radius;
-                type = chrono::collision::SPHERE;
-            }
-
-            else if (std::dynamic_pointer_cast<ChEllipsoidShape>(asset)) {
-                ChEllipsoidShape* ellipsoid_shape = ((ChEllipsoidShape*)(asset.get()));
-                rad_final = ellipsoid_shape->GetEllipsoidGeometry().rad;
-                type = chrono::collision::ELLIPSOID;
-            } else if (std::dynamic_pointer_cast<ChBoxShape>(asset)) {
-                ChBoxShape* box_shape = ((ChBoxShape*)(asset.get()));
-                rad_final = box_shape->GetBoxGeometry().Size;
-                type = chrono::collision::BOX;
-            } else if (std::dynamic_pointer_cast<ChCylinderShape>(asset)) {
-                ChCylinderShape* cylinder_shape = ((ChCylinderShape*)(asset.get()));
-                double rad = cylinder_shape->GetCylinderGeometry().rad;
-                double height = cylinder_shape->GetCylinderGeometry().p1.y - cylinder_shape->GetCylinderGeometry().p2.y;
-                rad_final.x = rad;
-                rad_final.y = height;
-                rad_final.z = rad;
-                type = chrono::collision::CYLINDER;
-            } else if (std::dynamic_pointer_cast<ChConeShape>(asset)) {
-                ChConeShape* cone_shape = ((ChConeShape*)(asset.get()));
-                rad_final.x = cone_shape->GetConeGeometry().rad.x;
-                rad_final.y = cone_shape->GetConeGeometry().rad.y;
-                rad_final.z = cone_shape->GetConeGeometry().rad.z;
-                type = chrono::collision::CONE;
-            }
-
-            csv_output << pos_final;
-            csv_output << lrot;
-            csv_output << vel;
-
-            if (std::dynamic_pointer_cast<ChSphereShape>(asset)) {
-                csv_output << type;
-                csv_output << rad_final.x;
-                csv_output.endline();
-            } else if (std::dynamic_pointer_cast<ChEllipsoidShape>(asset)) {
-                csv_output << type;
-                csv_output << real3(rad_final.x, rad_final.y, rad_final.z);
-                csv_output.endline();
-            } else if (std::dynamic_pointer_cast<ChBoxShape>(asset)) {
-                csv_output << type;
-                csv_output << real3(rad_final.x, rad_final.y, rad_final.z);
-                csv_output.endline();
-            } else if (std::dynamic_pointer_cast<ChCylinderShape>(asset)) {
-                csv_output << type;
-                csv_output << real2(rad_final.x, rad_final.y);
-                csv_output.endline();
-            } else if (std::dynamic_pointer_cast<ChConeShape>(asset)) {
-                csv_output << type;
-                csv_output << real2(rad_final.x, rad_final.y);
-                csv_output.endline();
-            } else {
-                csv_output << -1;
-                csv_output.endline();
-            }
-        }
-    }
-    DumpStringData(filename, csv_output.ss.str());
-}
+//class CSVGen {
+//  public:
+//    CSVGen() {
+//        delim = ",";
+//        binary = false;
+//        gz_file = 0;
+//    }
+//    ~CSVGen() {}
+//
+//    void OpenFile(std::string filename, bool bin = false) {
+//        if (bin) {
+//            binary = true;
+//            gz_file = gzopen(filename.c_str(), "wb");
+//        } else {
+//            ofile.open(filename.c_str(), std::ios::out);
+//        }
+//    }
+//
+//    void CloseFile() {
+//        if (binary) {
+//            unsigned long int file_size = sizeof(char) * ss.str().size();
+//
+//            gzwrite(gz_file, (void*)&file_size, sizeof(file_size));  // writing size of file
+//            gzwrite(gz_file, (void*)(ss.str().data()), file_size);
+//            gzclose(gz_file);
+//        } else {
+//            ofile << ss.str();
+//            ofile.close();
+//        }
+//    }
+//    template <class T>
+//    void operator<<(const T& token) {
+//        WriteToken(token);
+//    }
+//    void WriteToken(real token) { ss << token << delim; }
+//    void WriteToken(real2 token) { ss << token.x << delim << token.y << delim; }
+//    void WriteToken(real3 token) { ss << token.x << delim << token.y << delim << token.z << delim; }
+//    void WriteToken(real4 token) { ss << token.x << delim << token.y << delim << token.z << delim << token.w << delim; }
+//    void WriteToken(quaternion token) {
+//        ss << token.w << delim << token.x << delim << token.y << delim << token.z << delim;
+//    }
+//    void WriteToken(ChVector<> token) { ss << token.x << delim << token.y << delim << token.z << delim; }
+//    void WriteToken(ChQuaternion<> token) {
+//        ss << token.e0 << delim << token.e1 << delim << token.e2 << delim << token.e3 << delim;
+//    }
+//    void WriteToken(std::string token) { ss << token << delim; }
+//    void endline() { ss << std::endl; }
+//
+//    std::string delim;
+//    std::ofstream ofile;
+//    std::stringstream ss;
+//    gzFile gz_file;
+//    bool binary;
+//};
+//
+//void DumpAllObjectsWithGeometryPovray(ChSystem* mSys, std::string filename, bool binary = false) {
+//    CSVGen csv_output;
+//
+//    for (int i = 0; i < mSys->Get_bodylist()->size(); i++) {  //
+//        auto abody = mSys->Get_bodylist()->at(i);             // Get body
+//        const Vector pos = abody->GetFrame_REF_to_abs().GetPos();
+//        const Vector vel = abody->GetPos_dt();  // Get velocity
+//        Quaternion rot = abody->GetFrame_REF_to_abs().GetRot();
+//        Vector pos_final, rad_final;                 //
+//        ShapeType type = chrono::collision::SPHERE;  //
+//        // Get each asset
+//        for (int j = 0; j < abody->GetAssets().size(); j++) {
+//            auto asset = abody->GetAssets().at(j);
+//            if (!std::dynamic_pointer_cast<ChVisualization>(asset)) {
+//                continue;
+//            }
+//            ChVisualization* visual_asset = ((ChVisualization*)(asset.get()));
+//            Vector center = visual_asset->Pos;
+//            center = rot.Rotate(center);
+//            pos_final = pos + center;
+//            Quaternion lrot = visual_asset->Rot.Get_A_quaternion();
+//            lrot = rot * lrot;
+//            lrot.Normalize();
+//            if (std::dynamic_pointer_cast<ChSphereShape>(asset)) {
+//                ChSphereShape* sphere_shape = ((ChSphereShape*)(asset.get()));
+//                real radius = sphere_shape->GetSphereGeometry().rad;
+//                rad_final.x = radius;
+//                rad_final.y = radius;
+//                rad_final.z = radius;
+//                type = chrono::collision::SPHERE;
+//            }
+//
+//            else if (std::dynamic_pointer_cast<ChEllipsoidShape>(asset)) {
+//                ChEllipsoidShape* ellipsoid_shape = ((ChEllipsoidShape*)(asset.get()));
+//                rad_final = ellipsoid_shape->GetEllipsoidGeometry().rad;
+//                type = chrono::collision::ELLIPSOID;
+//            } else if (std::dynamic_pointer_cast<ChBoxShape>(asset)) {
+//                ChBoxShape* box_shape = ((ChBoxShape*)(asset.get()));
+//                rad_final = box_shape->GetBoxGeometry().Size;
+//                type = chrono::collision::BOX;
+//            } else if (std::dynamic_pointer_cast<ChCylinderShape>(asset)) {
+//                ChCylinderShape* cylinder_shape = ((ChCylinderShape*)(asset.get()));
+//                double rad = cylinder_shape->GetCylinderGeometry().rad;
+//                double height = cylinder_shape->GetCylinderGeometry().p1.y - cylinder_shape->GetCylinderGeometry().p2.y;
+//                rad_final.x = rad;
+//                rad_final.y = height;
+//                rad_final.z = rad;
+//                type = chrono::collision::CYLINDER;
+//            } else if (std::dynamic_pointer_cast<ChConeShape>(asset)) {
+//                ChConeShape* cone_shape = ((ChConeShape*)(asset.get()));
+//                rad_final.x = cone_shape->GetConeGeometry().rad.x;
+//                rad_final.y = cone_shape->GetConeGeometry().rad.y;
+//                rad_final.z = cone_shape->GetConeGeometry().rad.z;
+//                type = chrono::collision::CONE;
+//            }
+//
+//            csv_output << pos_final;
+//            csv_output << lrot;
+//            csv_output << vel;
+//
+//            if (std::dynamic_pointer_cast<ChSphereShape>(asset)) {
+//                csv_output << type;
+//                csv_output << rad_final.x;
+//                csv_output.endline();
+//            } else if (std::dynamic_pointer_cast<ChEllipsoidShape>(asset)) {
+//                csv_output << type;
+//                csv_output << real3(rad_final.x, rad_final.y, rad_final.z);
+//                csv_output.endline();
+//            } else if (std::dynamic_pointer_cast<ChBoxShape>(asset)) {
+//                csv_output << type;
+//                csv_output << real3(rad_final.x, rad_final.y, rad_final.z);
+//                csv_output.endline();
+//            } else if (std::dynamic_pointer_cast<ChCylinderShape>(asset)) {
+//                csv_output << type;
+//                csv_output << real2(rad_final.x, rad_final.y);
+//                csv_output.endline();
+//            } else if (std::dynamic_pointer_cast<ChConeShape>(asset)) {
+//                csv_output << type;
+//                csv_output << real2(rad_final.x, rad_final.y);
+//                csv_output.endline();
+//            } else {
+//                csv_output << -1;
+//                csv_output.endline();
+//            }
+//        }
+//    }
+//    DumpStringData(filename, csv_output.ss.str());
+//}
 
 #endif
--- sls.cpp
+++ sls.cpp
@@ -2,11 +2,21 @@
 #include <vector>
 #include <cmath>
 #include <string>
+#include <iostream>
 
-#include "common/input_output.h"
+#include "chrono_parallel/physics/ChSystemParallel.h"
+#include "chrono/utils/ChUtilsCreators.h"
+#include "chrono/utils/ChUtilsGenerators.h"
+
+#ifdef CHRONO_OPENGL
+#include "chrono_opengl/ChOpenGLWindow.h"
+#endif
 
 using std::cout;
 using std::endl;
+
+using namespace chrono;
+using namespace chrono::collision;
 
 // all dimensions are in millimeters, milligrams
 real container_width = 2.5;      // width of area with particles
@@ -26,11 +36,11 @@ real roller_velocity = -127;
 real roller_mass = 1;
 real roller_friction = .1;
 real roller_cohesion = 0;
-real particle_radius = .058 / 2.0;
+real particle_radius = 10 * .058 / 2.0; //.058 / 2.0;
 real particle_std_dev = .015 / 2.0;
 real particle_mass = .05;
 real particle_density = 0.93;
-real particle_layer_thickness = particle_radius * 32;
+real particle_layer_thickness = 0.928; // particle_radius * 32;
 real particle_friction = .52;
 real rolling_friction = .1;
 real spinning_friction = .1;
@@ -56,12 +66,12 @@ void RunTimeStep(T* mSys, const int frame) {
     ChVector<> roller_pos = ROLLER->GetPos();
 
     ROLLER->SetPos(ChVector<>(0, roller_radius + particle_layer_thickness + container_thickness,
-                              roller_pos.z + roller_velocity * timestep));
+                              roller_pos.z() + roller_velocity * timestep));
     ROLLER->SetPos_dt(ChVector<>(0, 0, roller_velocity));
     roller_omega = roller_velocity / roller_radius;
     ang += roller_omega * timestep;
     if (ang >= 2 * CH_C_PI) {
-        ang = 0;
+        ang -= 2 * CH_C_PI;
     }
 
     Quaternion q1;
@@ -75,7 +85,7 @@ void RunTimeStep(T* mSys, const int frame) {
     ROLLER->SetRot(q1 % roller_quat);
     ROLLER->SetWvel_loc(Vector(0, roller_omega, 0));
 
-    cout << "step " << frame << " " << ROLLER->GetPos().z << "\n";
+    cout << "step " << frame << " " << ROLLER->GetPos().z() << "\n";
 }
 
 int main(int argc, char* argv[]) {
@@ -87,14 +97,13 @@ int main(int argc, char* argv[]) {
 
     num_steps = time_end / timestep;
 
-    ChSystemParallelDVI* system = new ChSystemParallelDVI;
+	ChSystemParallelNSC* system = new ChSystemParallelNSC;
     system->Set_G_acc(ChVector<>(0, gravity, 0));
-    system->SetIntegrationType(ChSystem::INT_ANITESCU);
+    system->SetTimestepperType(ChTimestepper::Type::EULER_IMPLICIT_LINEARIZED);
 
     system->GetSettings()->min_threads = 8;
-
     system->GetSettings()->solver.tolerance = tolerance;
-    system->GetSettings()->solver.solver_mode = SPINNING;
+    system->GetSettings()->solver.solver_mode = SolverMode::SPINNING;
     system->GetSettings()->solver.max_iteration_normal = 30;
     system->GetSettings()->solver.max_iteration_sliding = max_iteration;
     system->GetSettings()->solver.max_iteration_spinning = max_iteration;
@@ -108,18 +117,18 @@ int main(int argc, char* argv[]) {
     system->GetSettings()->collision.aabb_max = real3(4.50145, 77.3794, 75.8014);
     system->GetSettings()->collision.aabb_min = real3(-4.50145, -0.125, -25.0014);
     system->GetSettings()->collision.use_aabb_active = true;
-    system->ChangeSolverType(BB);
-    system->SetLoggingLevel(LOG_INFO);
-    system->SetLoggingLevel(LOG_TRACE);
+    system->ChangeSolverType(SolverType::BB);
+	system->SetLoggingLevel(LoggingLevel::LOG_TRACE, true);
+	system->SetLoggingLevel(LoggingLevel::LOG_INFO, true);
 
     system->GetSettings()->collision.collision_envelope = particle_radius * .05;
     system->GetSettings()->collision.bins_per_axis = vec3(40, 300, 400);
-    system->GetSettings()->collision.narrowphase_algorithm = NARROWPHASE_HYBRID_MPR;
+    system->GetSettings()->collision.narrowphase_algorithm = NarrowPhaseType::NARROWPHASE_HYBRID_MPR;
     system->GetSettings()->collision.fixed_bins = true;
 
-    auto material_plate = std::make_shared<ChMaterialSurface>();
+    auto material_plate = std::make_shared<ChMaterialSurfaceNSC>();
     material_plate->SetFriction(0);
-    std::shared_ptr<ChBody> PLATE = std::make_shared<ChBody>(new ChCollisionModelParallel);
+	auto PLATE = std::make_shared<ChBody>(std::make_shared<ChCollisionModelParallel>());
     utils::InitializeObject(PLATE, 100000, material_plate, ChVector<>(0, 0, 0), QUNIT, true, true, 2, 6);
 
     utils::AddBoxGeometry(PLATE.get(), Vector(container_thickness, container_height, container_length),
@@ -133,20 +142,20 @@ int main(int argc, char* argv[]) {
     utils::AddBoxGeometry(PLATE.get(), Vector(container_width, container_thickness, container_length),
                           Vector(0, container_height * 2, 0));
 
-    FinalizeObject(PLATE, (ChSystemParallel*)system);
+    utils::FinalizeObject(PLATE, (ChSystemParallel*)system);
 
-    auto material_bottom = std::make_shared<ChMaterialSurface>();
+    auto material_bottom = std::make_shared<ChMaterialSurfaceNSC>();
     material_bottom->SetFriction(floor_friction);
-    std::shared_ptr<ChBody> BOTTOM = std::make_shared<ChBody>(new ChCollisionModelParallel);
+	auto BOTTOM = std::make_shared<ChBody>(std::make_shared<ChCollisionModelParallel>());
     utils::InitializeObject(BOTTOM, 100000, material_bottom, ChVector<>(0, 0, 0), QUNIT, true, true, 2, 6);
     utils::AddBoxGeometry(BOTTOM.get(), ChVector<>(container_width, container_thickness, container_length));
-    FinalizeObject(BOTTOM, (ChSystemParallel*)system);
+    utils::FinalizeObject(BOTTOM, (ChSystemParallel*)system);
 
-    ROLLER = std::make_shared<ChBody>(new ChCollisionModelParallel);
+	auto ROLLER = std::make_shared<ChBody>(std::make_shared<ChCollisionModelParallel>());
     ChQuaternion<> roller_quat;
     roller_quat.Q_from_AngAxis(CH_C_PI / 2.0, ChVector<>(0, 0, 1));
 
-    auto material_roller = std::make_shared<ChMaterialSurface>();
+    auto material_roller = std::make_shared<ChMaterialSurfaceNSC>();
     material_roller->SetFriction(roller_friction);
 
     utils::InitializeObject(ROLLER, 100000, material_roller,
@@ -154,27 +163,27 @@ int main(int argc, char* argv[]) {
                             roller_quat, true, false, 6, 6);
 
     utils::AddCylinderGeometry(ROLLER.get(), roller_radius, roller_length * 2);
-    FinalizeObject(ROLLER, (ChSystemParallel*)system);
+    utils::FinalizeObject(ROLLER, (ChSystemParallel*)system);
 
-    auto material_granular = std::make_shared<ChMaterialSurface>();
+    auto material_granular = std::make_shared<ChMaterialSurfaceNSC>();
     material_granular->SetFriction(particle_friction);
     material_granular->SetRollingFriction(rolling_friction);
     material_granular->SetSpinningFriction(spinning_friction);
 
     utils::Generator* gen = new utils::Generator(system);
 
-    std::shared_ptr<MixtureIngredient>& m1 = gen->AddMixtureIngredient(utils::SPHERE, 1);
+    std::shared_ptr<utils::MixtureIngredient>& m1 = gen->AddMixtureIngredient(utils::SPHERE, 1);
     m1->setDefaultSize(particle_radius);
     m1->setDefaultDensity(particle_density);
     m1->setDistributionSize(particle_radius, particle_std_dev, particle_radius - particle_std_dev,
                             particle_radius + particle_std_dev);
-    m1->setDefaultMaterialDVI(material_granular);
+    m1->setDefaultMaterial(material_granular);
 
     gen->createObjectsBox(utils::HCP_PACK, (particle_radius + particle_std_dev) * 2, ChVector<>(0, 1.0 + particle_layer_thickness*.5, 0),
                           ChVector<>(container_width - container_thickness * 2.5, particle_layer_thickness,
                                      container_length - container_thickness * 2.5));
 
-#if 0
+#ifdef CHRONO_OPENGL
     opengl::ChOpenGLWindow& gl_window = opengl::ChOpenGLWindow::getInstance();
     gl_window.Initialize(1280, 720, "Bucky", system);
     gl_window.SetCamera(ChVector<>(0, 0, -10), ChVector<>(0, 0, 0), ChVector<>(0, 1, 0), 0.1);
@@ -202,7 +211,7 @@ int main(int argc, char* argv[]) {
         system->DoStepDynamics(timestep);
         if (sim_frame == next_out_frame) {
             std::cout << "write: " << out_frame << std::endl;
-            DumpAllObjectsWithGeometryPovray(system, data_output_path + "data_" + std::to_string(out_frame) + ".dat",
+			utils::WriteShapesPovray(system, data_output_path + "data_" + std::to_string(out_frame) + ".dat",
                                              true);
             out_frame++;
             next_out_frame += out_steps;
